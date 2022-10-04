8e95033fecf219099f494b7062f4a0130f63c600
sopadelletres01 <marcportero01@gmail.com>
2022-10-04 21:11:19 +0200

    first try to make a good output to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Enter the darkness',
+      template:"index.html"
+    }),
+  ],
   resolve: {
     extensions: [".webpack.js", ".web.js", ".ts", ".js", ".css"],
   },
