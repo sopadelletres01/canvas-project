ec9afba4363c707dfcbeb14a298951615a6c113d
sopadelletres01 <marcportero01@gmail.com>
2022-10-04 21:26:51 +0200

    third try to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require("path");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.ts",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Enter the darkness',
-      template:"index.html"
+      title: "Enter the darkness",
+      template: "index.html",
     }),
+    new MiniCssExtractPlugin(),
   ],
   resolve: {
     extensions: [".webpack.js", ".web.js", ".ts", ".js", ".css"],
@@ -18,7 +21,7 @@ module.exports = {
   module: {
     rules: [
       { test: /\.ts$/, loader: "ts-loader" },
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/i, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
   },
 };
